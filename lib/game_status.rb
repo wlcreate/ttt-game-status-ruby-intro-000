d9822bcd7423f9b09f162248f3726f0afda4290a
Waverley Leung <waverley.nyc@gmail.com>
2020-03-19 22:08:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,11 +63,7 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == "X"
-    return "X"
-  elsif won?(board) == "O"
-    return "O"
-  else !won?(board)
-    return nil
+  if won?(board)
+    return board[won?(board)[0]]
   end
 end
